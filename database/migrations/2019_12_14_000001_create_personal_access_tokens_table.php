33bacce17d1657dc91bbed7a613ac303761877b5
ozwinghk <127014159+ozwinghk@users.noreply.github.com>
2023-08-13 22:07:49 +1000

    add certificate

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    /*public function up(): void
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
@@ -26,7 +26,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    /*public function down(): void
     {
         Schema::dropIfExists('personal_access_tokens');
     }
