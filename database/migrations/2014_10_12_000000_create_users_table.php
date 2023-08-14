1e75a4cfb978b4301d949bc6e6e68dae1843648c
ozwinghk <127014159+ozwinghk@users.noreply.github.com>
2023-08-14 10:33:46 +1000

    add certificate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,8 +25,5 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
-    }
+    
 };
